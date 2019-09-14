bef03ab3dde55a23026bda9ae949232ceb33633f
jishojohn <jishoprince001@gmail.com>
2019-09-14 10:59:45 +0400

    make it better

--- routes/Post.js
+++ routes/Post.js
@@ -1,42 +1,46 @@
-const express = require('express')
-const Post = require('../models/Post')
-
+const User = require('../models/User')
+const Post=require('../models/Post')
+const express=require('express')
 const router = express.Router();
 
 /**
  * Post route for register a new user
- * 
- * @name POST /users/regiser
- * 
- * @param {string} message - password
- * @param {string} email - password of the user
-
+ *  @name POST /users/regiser
+ * @param {string} email - email of the user
+ * @param {string} password - password of the user
+ * @param {name} name - name of the user
  */
+router.post('/posting', (req, res) => {
+    console.log(req.body.email);
+        User
+        .findOne({email:req.body.email})
+        .then(user=>{
+            if(user)
+            {
+                console.log(user);
+                const newPost = new Post({ 
+                 email:  req.body.email,     
+                message: req.body.message
+                })
+                newPost
+                .save()
+                .then(post => {
+                 res.json(post)
+                })
+                .catch(err => {
+                    res.json(err)
+                })
+            }
+            else 
+            {
 
-    router.post('/posting', (req, res) =>{
-        const newPost = new Post({
-   
-        message: req.body.message,
-        email: req.body.email
-        
-    })
-
-    newPost
-        .save()
-        .then(post=> {
-            res.json(post)
-        })
-        .catch(err=> {
-            res.json(err)
+            }
         })
 });
-
- 
-
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     Post.find()
-    .then(posts=>res.json(posts))
-    .catch(err=>res.json(err));
+        .then(posts => res.json(posts))
+        .catch(err => res.json(err));
 });
 
 
